662bc961c68189b35c889ca7cfb27d077466880f
TshireletsoMogwere <tmogwere09@gmail.com>
2024-10-21 18:13:37 +0200

    creates single recipe endpoint

--- db.js
+++ db.js
@@ -1,24 +1,25 @@
-const { MongoClient } = require('mongodb');
+import {MongoClient} from './MongoClient';
 
 // Replace with your MongoDB URI
-const uri = 'mongodb+srv://group-d:p0FlVQ6DhVbkZrYV@cluster0.knrq5.mongodb.net/'; // For local DB
+const uri =
+  "mongodb+srv://group-d:p0FlVQ6DhVbkZrYV@cluster0.knrq5.mongodb.net/"; // For local DB
 // Or, for MongoDB Atlas: 'mongodb+srv://<username>:<password>@cluster.mongodb.net/<dbname>?retryWrites=true&w=majority'
 
 const client = new MongoClient(uri, {
   maxPoolSize: 50,
   connectTimeoutMS: 5000,
-  retryWrites: true
+  retryWrites: true,
 });
 
 async function connectToDatabase() {
   try {
     await client.connect();
-    console.log('Connected to MongoDB!');
-    return client.db('devdb'); // Replace with your DB name
+    console.log("Connected to MongoDB!");
+    return client.db("devdb"); // Replace with your DB name
   } catch (error) {
-    console.error('MongoDB connection error:', error);
+    console.error("MongoDB connection error:", error);
     throw error;
   }
 }
 
-module.exports = connectToDatabase;
+export default connectToDatabase;
