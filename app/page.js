b0d55e8d61f92f779051432242263c604a3ed138
KeabetsweMatlhabe <157456088+KeabetsweMatlhabe@users.noreply.github.com>
2024-10-22 10:36:28 +0200

    installed tailwind css

--- app/page.js
+++ app/page.js
@@ -1,6 +1,36 @@
-export default function Home(){
-    return (
-        <>
-        </>
-    )
-}
+'use client';
+import React, { useEffect, useState } from 'react';
+import RecipeGrid from './components/recipeGrid';
+
+const HomePage = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchRecipes() {
+      try {
+        const response = await fetch('/api/recipes?page=1&limit=20');
+        const data = await response.json();
+        setRecipes(data.recipes);
+      } catch (error) {
+        console.error('Failed to fetch recipes:', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchRecipes();
+  }, []);
+
+  return (
+    <div className="container mx-auto">
+      {loading ? (
+        <p>Loading recipes...</p>
+      ) : (
+        <RecipeGrid recipes={recipes} />
+      )}
+    </div>
+  );
+};
+
+export default HomePage;
