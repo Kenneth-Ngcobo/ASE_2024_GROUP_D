e47c8c3728ed6af90bdab081291348887a3a600c
TshireletsoMogwere <tmogwere09@gmail.com>
2024-10-22 09:01:24 +0200

    modifies configuration

--- db.js
+++ db.js
@@ -1,25 +1,24 @@
-import {MongoClient} from './MongoClient';
+const { MongoClient } = require('mongodb');
 
 // Replace with your MongoDB URI
-const uri =
-  "mongodb+srv://group-d:p0FlVQ6DhVbkZrYV@cluster0.knrq5.mongodb.net/"; // For local DB
+const uri = 'mongodb+srv://group-d:p0FlVQ6DhVbkZrYV@cluster0.knrq5.mongodb.net/'; // For local DB
 // Or, for MongoDB Atlas: 'mongodb+srv://<username>:<password>@cluster.mongodb.net/<dbname>?retryWrites=true&w=majority'
 
 const client = new MongoClient(uri, {
   maxPoolSize: 50,
   connectTimeoutMS: 5000,
-  retryWrites: true,
+  retryWrites: true
 });
 
 async function connectToDatabase() {
   try {
     await client.connect();
-    console.log("Connected to MongoDB!");
-    return client.db("devdb"); // Replace with your DB name
+    console.log('Connected to MongoDB!');
+    return client.db('devdb'); // Replace with your DB name
   } catch (error) {
-    console.error("MongoDB connection error:", error);
+    console.error('MongoDB connection error:', error);
     throw error;
   }
 }
 
-export default connectToDatabase;
+module.exports = connectToDatabase;
