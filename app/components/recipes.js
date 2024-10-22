a01e9cc7deb50192284768276091b3dd9a2fe1cd
Thabang Rammitlwa <thabang451@gmail.com>
2024-10-22 11:14:43 +0200

    Updated Recipes component and Home page to handle loading and error states, and added API call to fetch recipes

--- app/components/recipes.js
+++ app/components/recipes.js
@@ -1,36 +1,38 @@
+'use client'
 
-
-import Link from "next/link"
-import { useState, useEffect } from "react"
-
-
+import Link from 'next/link';
+import { useState, useEffect } from 'react';
 
 export default function Recipes({ recipes }) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  
+
   useEffect(() => {
-    setLoading(false);
-    }, [recipes])
+    if (recipes) {
+      setLoading(false);
+    }
+  }, [recipes]);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div className="text-red-500 text-center p-4">Error: {error}</div>;
 
   return (
     <div>
       <h1>Recipes</h1>
       <div>
-        {recipes.map((recipe) => (
-          <Link key={recipe.id}>
-            <h2>{recipe.title}</h2>
-            <p>Published:{new Date(recipe.published).toDateString()}</p>
-            <p><strong>Prep Time:</strong>{ recipe.prep} minutes</p>
-            <p><strong>Cook Time:</strong>{ recipe.cook } minutes</p>
-            <p><strong>Servings:</strong>{ recipe.servings}</p>
-            <p><strong>Category</strong>{recipe.Category}</p>
+        {recipes && recipes.map((recipe) => (
+          <Link href={`/recipes/${recipe.id}`} key={recipe.id}>
+            
+              <h2>{recipe.title}</h2>
+              <p>Published: {new Date(recipe.published).toDateString()}</p>
+              <p><strong>Prep Time:</strong> {recipe.prep} minutes</p>
+              <p><strong>Cook Time:</strong> {recipe.cook} minutes</p>
+              <p><strong>Servings:</strong> {recipe.servings}</p>
+              <p><strong>Category:</strong> {recipe.category}</p>
           
-          </Link> 
-        
-       ))}
+          </Link>
+        ))}
       </div>
-         </div>
-   
-  )
+    </div>
+  );
 }
