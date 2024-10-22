f59f65f8fda23d82035ee95229ad77b2a526afce
Tumelo Mogono <tumelomogono@gmail.com>
2024-10-22 16:46:33 +0200

    Changed the initial recipes to an object and added pagination.

--- app/page.js
+++ app/page.js
@@ -1,20 +1,29 @@
 import { fetchRecipes } from './api'
+import Pagination from './components/pagination';
 import Recipes from './components/recipes';
 
-export default async function Home() {
-  let recipes = [];
-  let error = null;
+export default async function Home({ searchParams}) {
+    let recipes = {};
+    let error = null;
 
-  try {
-    recipes = await fetchRecipes();
-  } catch (e) {
-    error = e.message;
-  }
+    const page = searchParams.page ? parseInt(searchParams.page) : 1;
 
-  return (
-    <>
-      <Recipes recipes={recipes} />
-      {error && <div className="text-red-500 text-center p-4">Error: {error}</div>}
-    </>
-  );
+    try {
+        recipes = await fetchRecipes(20, page );
+    } catch (e) {
+        error = e.message;
+    }
+
+
+
+    return (
+        <>
+            <Recipes recipes={recipes.recipes} />
+            <Pagination 
+                currentPage={recipes.currentPage}
+                totalPages={recipes.totalPages}
+            />
+            {error && <div className="text-red-500 text-center p-4">Error: {error}</div>}
+        </>
+    );
 }
