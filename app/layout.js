caf3803f28bed736210a6ba22681a74362b4eef8
TshireletsoMogwere <tmogwere09@gmail.com>
2024-10-23 14:01:05 +0200

    merging branches

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "./components/Header";
+import Footer from "./components/footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -56,6 +57,7 @@ export default function RootLayout({ children }) {
       >
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
