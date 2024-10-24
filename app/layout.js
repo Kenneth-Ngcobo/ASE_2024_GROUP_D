e9c6cb9a9e7434051970051d7de0c93e4b425324
TapiwaHerbetNosenga <144127869+TapiwaHerbetNosenga@users.noreply.github.com>
2024-10-24 12:40:28 +0200

    I have changed the main meta tags for the project, updated the favicon and changed the title of the website.

--- app/layout.js
+++ app/layout.js
@@ -22,31 +22,31 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <title>Recipe website</title>
-        <link rel="icon" href="restaurant.png" />
+        <title>KwaMai Eatery</title>
+        <link rel="icon" href="/Kwa.jpg" />
 
         {/* Main Meta Tags */}
     
 
-        <meta name="description" content="This is a recipe website" />
+        <meta name="description" content="This is the official recipe website of KwaMai Eatery" />
         <meta
           name="keywords"
-          content="Food, recipe, dinner, lunch, snack, breakfast, vegan, ingredients "
+          content="Food, recipe, dinner, lunch, snack, ingredients, kwamai, eatery "
         />
         <meta name="author" content="Codespace" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         {/* Open Graph Meta Tags */}
-        <meta property="og:title" content="Recipe Website" />
-        <meta property="og:description" content="This is a recipe website" />
-        <meta property="og:image" content="/knifefork.png" />
-        <meta property="og:url" content="To be added" />
+        <meta property="og:title" content="KwaMai Eatery" />
+        <meta property="og:description" content="This is the official recipe website of KwaMai Eatery" />
+        <meta property="og:image" content="/kwaMai.jpg" />
+        <meta property="og:url" content="To be added when app is launched" />
 
         {/* Twitter Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Recipe Website" />
-        <meta name="twitter:description" content="This is a recipe website" />
-        <meta name="twitter:image" content="/knifefork.png" />
+        <meta name="twitter:title" content="KwaMai Eatery" />
+        <meta name="twitter:description" content="This is the official recipe website of KwaMai Eatery" />
+        <meta name="twitter:image" content="/kwaMai.jpg" />
       </head>
 
       <body
