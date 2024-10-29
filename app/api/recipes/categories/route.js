f58dda034fc82b40c4d3d6884edd16ea69818bd9
TshireletsoMogwere <tmogwere09@gmail.com>
2024-10-29 13:16:33 +0200

    fixes category endpoint to fetch from category collection

--- app/api/recipes/categories/route.js
+++ app/api/recipes/categories/route.js
@@ -1,30 +1,28 @@
-
 import connectToDatabase from "../../../../db";
 
 /**
- * Handles GET requests to fetch distinct categories from the recipes collection.
+ * Handles GET requests to fetch distinct categories from the categories collection.
  * 
  * @param {Request} req - The request object representing the incoming HTTP request.
  * @returns {Response} A JSON response containing an array of distinct categories or an error message.
  */
 export async function GET(req) {
     try {
-        
         const db = await connectToDatabase();
 
-        {/** fetch categories from the recipes collection */}
-        const categories = await db.collection('recipes').distinct('category');
+        {/** fetch categories from the categories collection */}
+        const categories = await db.collection('categories').distinct('categories');
 
-        {/**return a successful response with the categories in JSON format*/}
+          {/**return a successful response with the categories in JSON format*/}
         return new Response(JSON.stringify(categories), {
             status: 200,
             headers: { 'Content-Type': 'application/json' },
         });
     } catch (error) {
-        {/**log the error message for debugging*/}
+           {/**log the error message for debugging*/}
         console.error('Error fetching categories:', error.message);
 
-        {/**return an error response with status 500*/}
+           {/**return an error response with status 500*/}
         return new Response(JSON.stringify({ error: 'Failed to fetch categories' }), {
             status: 500,
             headers: { 'Content-Type': 'application/json' },
