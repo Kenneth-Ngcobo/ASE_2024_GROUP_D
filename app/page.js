bd23d3cece9d8969652fc2b6332b7e53f7bbc6e2
Tumelo Mogono <tumelomogono@gmail.com>
2024-10-30 13:32:30 +0200

    Imported suspense and the Loading handler to have it as a fallback while rendering data. Also refactored the catch for the error variable.

--- app/page.js
+++ app/page.js
@@ -2,7 +2,9 @@ import { fetchRecipes } from './api';
 import Pagination from './components/pagination';
 import Recipes from './components/recipes';
 import Footer from './components/footer';
-import Loading from './components/loading';
+import LoadingSpinner from './components/loadingSpinner';
+import Loading from './loading';
+import { Suspense } from 'react';
 
 export default async function Home({ searchParams }) {
     // Initialize recipes object to store fetched data
@@ -16,26 +18,25 @@ export default async function Home({ searchParams }) {
     try {
         // Fetch recipes from the API with a limit of 20 per page
         recipes = await fetchRecipes(20, page);
-    } catch (error) {
+    } catch (err) {
         // Capture any error that occurs during the fetch
-        error = error.message; // Store the error message
+        error = err.message; // Store the error message
     }
 
-    if(recipes.recipes.length === 0 && !error) {
-        return <Loading />;
-    }
 
     // Return the rendered JS for the Home component
     return (
         <>
-            {/* Render the Recipes component, passing the fetched recipes as props */}
-            <Recipes recipes={recipes.recipes} />
-            {/* Render the Pagination component, passing the current page and total pages */}
-            <Pagination
-                currentPage={recipes.currentPage} // Current page number
-                totalPages={recipes.totalPages} // Total number of pages available
-            />
-            <Footer />
+            <Suspense fallback={<Loading />}>
+                {/* Render the Recipes component, passing the fetched recipes as props */}
+                <Recipes recipes={recipes.recipes} />
+                {/* Render the Pagination component, passing the current page and total pages */}
+                <Pagination
+                    currentPage={recipes.currentPage} // Current page number
+                    totalPages={recipes.totalPages} // Total number of pages available
+                />
+                <Footer />
+            </Suspense>
         </>
     );
 }
