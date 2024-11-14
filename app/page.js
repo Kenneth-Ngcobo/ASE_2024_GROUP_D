ef1f436939507b2c71e148106bbc16178b366302
Tumelo Mogono <tumelomogono@gmail.com>
2024-11-14 11:36:23 +0200

    Removed unused imported component

--- app/page.js
+++ app/page.js
@@ -4,8 +4,6 @@ import Pagination from "./components/pagination";
 import Recipes from "./components/recipes";
 import Footer from "./components/footer";
 import Loading from "./loading";
-import SearchBar from "./components/searchBar";
-import CategoryList from "./components/CategoryList";
 import { Suspense } from 'react';
 
 export default async function Home({ searchParams }) {
