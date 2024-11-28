b474cd018d02a48b63643bd51354a31a206d3d20
TapiwaHerbetNosenga <144127869+TapiwaHerbetNosenga@users.noreply.github.com>
2024-11-28 11:02:51 +0200

    Fixed manifest warnings by updating icons

--- app/api/manifest/route.js
+++ app/api/manifest/route.js
@@ -1,24 +1,47 @@
 export async function GET(request) {
-    const manifest = {
-      name: 'Kwamai Eatery',
-      short_name: 'Next.js App',
-      description: 'Next.js App',
-      start_url: '/',
-      display: 'standalone',
-      background_color: '#fff',
-      theme_color: '#fff',
-      icons: [
-        {
-          src: '/Kwa.png',
-          sizes: '192x192',
-          type: 'image/png',
-        },
-      ],
-    };
-    return new Response(JSON.stringify(manifest), {
-      headers: {
-        'Content-Type': 'application/json',
+  const manifest = {
+    name: 'Kwamai Eatery',
+    short_name: 'Next.js App',
+    description: 'Next.js App',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#fff',
+    theme_color: '#fff',
+    icons: [
+      {
+        src: '/favicon/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
       },
-    });
-  }
-  
+      {
+        src: '/favicon/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: '/favicon/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+    screenshots: [
+      {
+        src: '/screenshot-wide.png',
+        sizes: '1024x487',
+        type: 'image/png',
+        form_factor: 'wide',
+      },
+      {
+        src: '/screenshot-mobile.png',
+        sizes: '447x800',
+        type: 'image/png',
+      },
+    ],
+  };
+
+  return new Response(JSON.stringify(manifest), {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
