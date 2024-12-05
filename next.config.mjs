6ab250c112c515c7fb1b5daeef861e5862e38a7f
K0BraKhan33 <mikaeel.gamieldien@gmail.com>
2024-12-05 09:49:11 +0200

    Duncan Pwa working version

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,28 @@
-/** @type {import('next').NextConfig} */
+import withPWAInit from "@ducanh2912/next-pwa";
+
+// Initialize PWA configuration
+const withPWA = withPWAInit({
+  dest: "public", // Directory where service worker files are stored
+  // Uncomment and customize options as needed
+   disable: process.env.NODE_ENV === "development",
+   register: true,
+   scope: "/app",
+   sw: "Service-Worker.js",
+});
+
+// Define your Next.js configuration
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: "img.sndimg.com",
-                port: "",
-                pathname: "/**"
-            }
-        ]
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.sndimg.com",
+        port: "",
+        pathname: "/**", // Allows any path from the hostname
+      },
+    ],
+  }
 };
 
-export default nextConfig;
+// Export the configuration wrapped with PWA
+export default withPWA(nextConfig);
